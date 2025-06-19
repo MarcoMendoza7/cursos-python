b08491ec1abc12c60d983cd3e3593e9fd25cbe2a
MarcoMendoza7 <mendozamarcope29@gmail.com>
2025-06-18 19:36:48 -0600

    COMMIT

--- variables/variables.py
+++ variables/variables.py
@@ -1,4 +1,10 @@
-#Variables simples, comun y corrientes..
+#VARIABLES.
+
+# Una variable es un espacio creado y reservado en memoria para despues 
+# ser utilizado por el progranador con una finalidad especifica.
+# Una variable puede reservar cualquier tipo de dato, ya sean cadenas de texto, numeros, decimales,
+#listas, tuplas, arreglos, etc, etc, etc. 
+#NOTA = :)
 
 nombre = "Marco"
 print (nombre)
