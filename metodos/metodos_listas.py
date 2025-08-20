a4da6179bb3f42328edcb572e4642de7b69279a9
MarcoMendoza7 <mendozamarcope29@gmail.com>
2025-08-20 00:27:30 -0600

    COMMIT

---
+++ metodos/metodos_listas.py
@@ -0,0 +1,17 @@
+# LISTAS = LIST 
+
+""" 
+
+LIST = crea una lista 
+
+LEN = cuenta la cantidad de elementos de una lista 
+
+APPEND = agrega un elemento a la lista 
+
+INSERT = agrega un elemento a la lista en el indice especificado 
+
+EXTEND = agrega varios elementos a la lista 
+
+
+
+"""
