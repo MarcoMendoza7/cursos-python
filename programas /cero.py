5730227010b107fe8bcd75cace3780eb90821034
MarcoMendoza7 <mendozamarcope29@gmail.com>
2025-08-21 00:28:11 -0600

    COMMIT

---
+++ programas /cero.py
@@ -0,0 +1,26 @@
+"""Haz un programa que:
+
+Pida el nombre y edad de la persona.
+Guarde en un objeto Persona.
+Lo meta en una lista.
+Recorra la lista y los salude.
+
+"""
+
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):
+        print(f"Hola, soy {self.nombre} y tengo {self.edad} años.")
+
+personas = []
+
+for i in range(3):
+    nombre = input("Nombre: ")
+    edad = int(input("Edad: "))
+    personas.append(Persona(nombre, edad))
+
+for p in personas:
+    p.saludar() 
